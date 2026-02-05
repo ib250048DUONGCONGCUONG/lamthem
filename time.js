eb33d4a850ffd1d865464058383338a8ef7b79eb
ib250048DUONGCONGCUONG <ib250048cuong@setouchicollege.com>
2026-02-05 11:22:59 +0900

    Add files via upload

--- time.js
+++ time.js
@@ -1,12 +1,26 @@
-function calculate() {
-  const hours = document.getElementById("hours").value;
-  const wage = document.getElementById("wage").value;
+let total = 0;
 
-  if (hours === "" || wage === "") {
-    alert("勤務時間と時給を入力してください");
+function addWork() {
+  const date = document.getElementById("date").value;
+  const start = document.getElementById("start").value;
+  const end = document.getElementById("end").value;
+  const salary = document.getElementById("salary").value;
+
+  if (!date || !start || !end || !salary) {
+    alert("全部入力してください");
     return;
   }
 
-  const total = hours * wage;
-  document.getElementById("total").textContent = total;
+  const startTime = new Date(`2024-01-01 ${start}`);
+  const endTime = new Date(`2024-01-01 ${end}`);
+  const hours = (endTime - startTime) / 3600000;
+
+  const money = hours * salary;
+  total += money;
+
+  const li = document.createElement("li");
+  li.textContent = `${date}｜${hours} 時間｜${money} 円`;
+  document.getElementById("list").appendChild(li);
+
+  document.getElementById("total").textContent = `合計：${total} 円`;
 }
